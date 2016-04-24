b1e966cacb5d02ef9d667b912e6549d7c82d9e1c
Christopher Stanley <chris@stanleynetworks.net>
2016-04-24 10:01:21 -0400

    Update batch_handbrake.sh

--- batch_handbrake.sh
+++ batch_handbrake.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-file=`ls`
-for i in $file;
-do
-  #Strip File Extensions
+
+file=`ls ../iso/`
+for i in $file; 
+ do
   fileNE=`echo $i | sed -e 's:\.[^./]*$::'`
   echo $fileNE
-  HandBrakeCLI.exe -i $i -o converted/$fileNE.mp4 --preset="High Profile"
-done
+  /cygdrive/c/Program\ Files/Handbrake/HandBrakeCLI.exe -i $i -t 1 --angle 1 -o F:\\converted\\$fileNE.mp4 -f mp4 --loose-anamorphic  --modulus 2 -e x264 -q 20 --cfr -a 1 -E av_aac -6 dpl2 -R 48 -B 160 -D 0 --gain 0 --audio-fallback ac3 --encoder-preset=medium  --encoder-level="4.1"  --encoder-profile=high  --verbose=1 -U
+ done
+
+times
